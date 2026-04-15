427f3c87bef8c69d82960540719b56a1e3227c1d
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-04-15 19:18:32 +0100

    feat(headscale): initial commit

---
+++ headscale.nix
@@ -0,0 +1,34 @@
+{
+  services.headscale = let
+    domain = "partlyaweso.me";
+    ts_domain = "on.${domain}";
+    https_ts_domain = "https://${ts_domain}";
+  in {
+    enable = true;
+    address = "0.0.0.0";
+    port = 64328;
+
+    settings = {
+      server_url = https_ts_domain;
+
+      dns = {
+        base_domain = domain;
+        search_domains = [ts_domain];
+        nameservers.global = [
+          # cloudflare ipv4
+          "1.1.1.1"
+          "1.0.0.1"
+          # cloudflare ipv6
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
+          # google ipv4
+          "8.8.8.8"
+          "8.8.4.4"
+          # google ipv6
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+        ];
+      };
+    };
+  };
+}
