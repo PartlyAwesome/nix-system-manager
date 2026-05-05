0552d1ab2eed80478ae6a8df939552f87f461729
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-05-06 00:15:05 +0100

    feat(caddy): tailscale plugin

--- caddy.nix
+++ caddy.nix
@@ -10,8 +10,9 @@
     package = pkgs.caddy.withPlugins {
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.2.4"
+        "github.com/tailscale/caddy-tailscale@v0.0.0-20260106222316-bb080c4414ac"
       ];
-      hash = "sha256-Olz4W84Kiyldy+JtbIicVCL7dAYl4zq+2rxEOUTObxA=";
+      hash = "sha256-LIcgZi32N0V5IGZPJbyI+n2YegXRjH/R/+lO2kzkCaI=";
     };
   };
 }
